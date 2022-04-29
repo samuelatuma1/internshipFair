0de1784de34f2a039c4fc2f00e071008ab6fb9b2
samuelatuma1 <atumasaake@gmail.com>
2022-04-29 07:36:33 +0100

    Added more fields for details, styled sign-up page

--- controller/auth.js
+++ controller/auth.js
@@ -3,12 +3,35 @@ const express = require("express")
 const auth = express.Router()
 const {check, validationResult} = require('express-validator')
 
-
+// Form validatons 
 const validateSignUpDetails = [
+    check("first_name").isLength({in : 2}).custom((val, {req}) => {
+         // Ensure first is not Just made up of ' '(space)
+         let startChar = ' '
+         for(let char of val){
+             if (char !== startChar){
+                 return val
+             }
+         }
+         throw new Error("Please enter a valid First Name")
+
+    }),
+    check("last_name").isLength({in : 2}).custom((val, {req}) => {
+        // Ensure first is not Just made up of ' '(space)
+        let startChar = ' '
+        for(let char of val){
+            if (char !== startChar){
+                return val
+            }
+        }
+        throw new Error("Please enter a valid Last Name")
+
+   }),
     check("email").isEmail().withMessage("Please, input a valid mail."),
     check("password").isLength({ min : 5}).withMessage("Password must be at least 3 characters Long"),
     check("password").custom((val, {req}) => {
         // Ensure password is not Just a repeat of the same characters
+
         let startChar = val[0]
         for(let char of val){
             if (char !== startChar){
@@ -24,6 +47,19 @@ const validateSignUpDetails = [
         }
     }) 
 ]
+// End of form validations
+
+
+// User decides if he/she is an intern or an employer
+auth.route('/')
+    .get((req, res) => {
+        res.type("html")
+        res.status(200).render('auth/role')
+    })
+
+
+
+// Signs up for Interns. !! Not employers
 auth.route('/signup')
     .get((req, res) => {
         res.type("html")
